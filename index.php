941c0b9fcdd11babacb833861f44f22b3b8e0e8f
Marcus Markman Riger <marc6388@edu.campusvejle.dk>
2017-06-15 11:48:53 +0200

    Starten på designed

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://use.fontawesome.com/50a72b3c2e.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css"> </head>
+
+<body>
+    <!-- Lidt selvhjælp -->
+    <div class="parent">
+        <div class="wrapper">
+            <!-- Headeren -->
+            <header><img src="header_img.jpg"></header>
+            <!-- Navbar -->
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Planter</a></li>
+                    <li><a href="#">Indretning</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">Log ind</a></li>
+                </ul>
+            </nav>
+            <!-- Selvhjælp -->
+            <div class="main">
+                <!-- Information -->
+                <div class="aboutMe"> <img alt="Lise" title="Lise Efternavn" src="frk_overspringshandling.jpg">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem accusamus odio placeat sapiente id minus obcaecati alias nulla numquam dicta sunt inventore totam, nobis, explicabo sed quae nam optio culpa!</p>
+                    <!-- Logo'er --><a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a> <a href=""><i class="fa fa-facebook-square" aria-hidden="true"></i></a> <a href=""><i class="fa fa-flickr" aria-hidden="true"></i></a> <a href=""><i class="fa fa-linkedin-square" aria-hidden="true"></i></a> </div>
+                <!-- Artikler PHP -->
+                <article>
+                    <h3>Vores nye lænestol i egetræ og kernelæder</h3> <img alt="lænestol" src="l%C3%A6nestol.jpg">
+                    <p>15-06-2017</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, consectetur. Asperiores hic cum laboriosam fuga officiis, molestiae, delectus soluta repellat, atque sint, nihil! Necessitatibus corrupti provident, aspernatur quidem nostrum vitae.</p>
+                </article>
+            </div>
+            <!-- Footer -->
+            <footer>
+                <p>&copy 2017 - Wilderness</p>
+            </footer>
+        </div>
+    </div>
+</body>
+
+</html>
